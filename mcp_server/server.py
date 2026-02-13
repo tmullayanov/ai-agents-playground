0bc07152a9121b0cf02a5f6e7041645727086f3e
Timur Mullayanov <tmullayanov@users.noreply.github.com>
2026-02-13 16:52:32 +0300

    Add demo mcp server

---
+++ mcp_server/server.py
@@ -0,0 +1,24 @@
+from fastmcp import FastMCP
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+mcp = FastMCP("Demo server!")
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    return a + b
+
+@mcp.custom_route(
+    "/health",
+    methods=["GET"],
+    include_in_schema=False
+)
+async def health(request: Request):
+    return JSONResponse({"status": "ok"})
+
+
+def run():
+    mcp.run(transport="streamable-http")
+
+if __name__ == "__main__":
+    run()
